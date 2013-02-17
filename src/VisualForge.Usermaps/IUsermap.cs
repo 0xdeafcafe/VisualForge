c7d06a911a57f5cbeb742d0489aca1bcb120ebc5
Xerax <alexreed@outlook.com>
2013-02-17 09:52:18 +0000

    Initial Project Backbone

---
+++ src/VisualForge.Usermaps/IUsermap.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+namespace VisualForge.Usermaps
+{
+	public interface IUsermap
+	{
+		void Initalize(Stream fileStream);
+
+		// Loading Code
+		void LoadHeader();
+		void LoadTags();
+		void LoadItemPlacement();
+		void LoadTagEntrys();
+
+		// Updateing Code
+		void Update();
+		void UpdateHeader();
+		void UpdateItemPlacement();
+		void UpdateTagEntries();
+
+		void Close();
+	}
+}
